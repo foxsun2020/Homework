49ff71296a42ac6370a96fb0520fd3a26a8cd150
foxsun2020 <yosen7@163.com>
2020-10-21 21:23:55 +0800

    Initial commit

---
+++ 1/Task_1.py
@@ -0,0 +1,21 @@
+import os
+import xlrd
+import re
+
+
+def main():
+    path = r'.\CollegeCode'  # open the folder that need to operate
+    for each in os.listdir(path):
+        if re.match(r'^[0-9]{1,2}\.xlsx$', each):  # check file name by re
+            file_name = os.path.splitext(each)[0]  # split file name and extract base name
+            xlsx = xlrd.open_workbook(r'.\CollegeCode\%s' % each)
+            sheet1 = xlsx.sheets()[0]
+            sheet1_nrow = sheet1.nrows  # the number of row
+            t = open(r'.\Output\%s.txt' % file_name, 'a', encoding='gbk')
+            t.write(str(sheet1.row_values(0)) + '\n')  # insert sheet header
+            for i in range(sheet1_nrow - 1, 0, -1):  # insert data
+                t.write(str(sheet1.row_values(i)) + '\n')
+            t.close()
+
+
+
---
+++ 2/Task_2.py
@@ -0,0 +1,26 @@
+import xlrd
+
+
+def check_major(c_path, s_path):
+    c_excel = xlrd.open_workbook(c_path)  # open Checklist.xlsx
+    s_excel = xlrd.open_workbook(s_path)  # open Standard.xlsx
+    c_sheet = c_excel.sheets()[0]
+    s_sheet = s_excel.sheets()[0]
+    c_nrow = c_sheet.nrows  # the number of row - Checklist.xlsx
+    s_nrow = s_sheet.nrows  # the number of row - Standard.xlsx
+    for a in range(1, c_nrow):
+        c_stu_id = c_sheet.row_values(a)[0]
+        name = c_sheet.row_values(a)[1]
+        c_major = c_sheet.row_values(a)[6]
+        for b in range(1, s_nrow):
+            s_stu_id = s_sheet.row_values(b)[0]
+            s_major = s_sheet.row_values(b)[6]
+            if c_stu_id == s_stu_id:
+                if c_major == s_major:
+                    continue
+                else:
+                    print('考生编号 %s 的 %s 应为 %s，误选为 %s' % (str(c_stu_id)[0:15], name, s_major, c_major))
+
+
+if __name__ == '__main__':
+    check_major(r'.\CheckList.xlsx', r'.\Standard.xlsx')
