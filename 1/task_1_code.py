49d260196a7968f4a1f51dc3e4dcd8e5e92fb21a
foxsun2020 <yosen7@163.com>
2020-10-24 08:47:54 +0800

    add time stamp in task-1

--- 1/task_1_code.py
+++ 1/task_1_code.py
@@ -12,9 +12,11 @@ import time
 
 def main():
     path = r'.\CollegeCode'  # open the folder that need to operate
+    counter = 0
+    time_stamp = time.strftime("%Y%m%d_%H%M%S", time.localtime())
     for each in os.listdir(path):
         if re.match(r"^[0-9]{1,2}\.xlsx$", each):  # check file name by re
-            file_name = str(time.strftime("%Y%m%d_%H%M_", time.localtime()) + os.path.splitext(each)[0])
+            file_name = str(time_stamp + '_' + os.path.splitext(each)[0])
             # add time-stamp in file name to prevent overwriting
             xlsx = xlrd.open_workbook(r'.\CollegeCode\%s' % each)
             sheet1 = xlsx.sheets()[0]
@@ -24,6 +26,8 @@ def main():
             for i in range(sheet1_nrows - 1, 0, -1):  # insert data
                 t.write(str(sheet1.row_values(i)) + '\n')
             t.close()
+            counter += 1
+    print('%s files finished with name started with *%s*' % (counter, time_stamp))
 
 
 if __name__ == '__main__':
