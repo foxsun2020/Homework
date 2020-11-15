561f8e15e4948807e26765c36a8d734485e3dc76
foxsun2020 <yosen7@163.com>
2020-11-15 10:06:56 +0800

    update var in task 4(math)

--- 4/math.py
+++ 4/math.py
@@ -6,6 +6,7 @@ Date: 2020/11/10
 """
 
 import numpy as np  # using version-1.19.3 if you are Windows PC
+import math
 
 
 def arr_input():
@@ -20,14 +21,32 @@ def arr_input():
     return array
 
 
+def sum_ln(ar):
+    sum = 0
+    for num in ar:
+        ln = math.log(num, math.e)
+        sum += ln
+    return sum
+
+
+def var(ar):
+    sum = 0
+    for num in ar:
+        dis = num - np.mean(ar)
+        sum += dis * dis
+    return sum/(len(ar) - 1)
+
+
 if __name__ == '__main__':
-    arr = arr_input()
+    # arr = arr_input()
+    arr = [2.14, 2.1, 2.13, 2.15, 2.13, 2.12, 2.13, 2.1, 2.15, 2.12, 2.14, 2.1, 2.13, 2.11, 2.14, 2.11]
     arr_mean = np.mean(arr)
-    arr_var = np.var(arr)
-    arr_std = np.std(arr)
+    arr_var = var(arr)
+    arr_std = var(arr) ** 0.5
     print("Mean: %f" % arr_mean)
-    print("Variance: %f" % arr_var)
-    print("Std_Variance: %f" % arr_std)
+    print("Variance (unbiased): %f" % arr_var)
+    print("Std_Variance (unbiased): %f" % arr_std)
+    # print(-(1+len(arr)/sum_ln(arr)))
 
 
 
